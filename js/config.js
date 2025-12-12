74825a1427e0f8a32a416ed57db00a067a840905
Slidj <78273709+Slidj@users.noreply.github.com>
2025-12-12 07:53:13 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
+// Налаштування та Ключі
+export const KEYS = {
+    NEWSDATA: "pub_22e4e8780f9349e7a64a65f886ecae3a",
+    GNEWS: "988894076e3186f8fbd93db235ee6fe9",
+    TMDB: "4dac8d33b5f9ef7b7c69d94b3f9cd56b"
+};
+
+export const API_URLS = {
+    newsdata: 'https://newsdata.io/api/1/news',
+    gnews: 'https://gnews.io/api/v4/search',
+    tmdb: 'https://api.themoviedb.org/3',
+    tmdbImg: 'https://image.tmdb.org/t/p/w500'
+};
 

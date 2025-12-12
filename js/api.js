d0dfc8c1b8e6dfa2be41d2ef78975156157a7ad1
Slidj <78273709+Slidj@users.noreply.github.com>
2025-12-12 07:54:35 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,28 @@
+import { KEYS, API_URLS } from './config.js';
+
+// --- ЗАПИТ ДО НОВИН ---
+export async function fetchNewsData(query, category, pageToken) {
+    let url = `${API_URLS.newsdata}?apikey=${KEYS.NEWSDATA}&language=uk&country=ua&size=10`;
+    if (query) url += `&q=${query}`;
+    if (category) url += `&category=${category}`;
+    if (pageToken) url += `&page=${pageToken}`;
+
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`News API Error ${res.status}`);
+    return await res.json();
+}
+
+// --- ЗАПИТ ДО TMDB ---
+export async function fetchTMDB(query, page) {
+    let url = `${API_URLS.tmdb}`;
+    if (query) {
+        url += `/search/movie?api_key=${KEYS.TMDB}&language=uk-UA&query=${query}&page=${page}`;
+    } else {
+        url += `/movie/popular?api_key=${KEYS.TMDB}&language=uk-UA&page=${page}`;
+    }
+
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`TMDB Error ${res.status}`);
+    return await res.json();
+}
 

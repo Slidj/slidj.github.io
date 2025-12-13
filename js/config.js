7852404be14638e334b65f1b9023b9fad4bdc376
Slidj <78273709+Slidj@users.noreply.github.com>
2025-12-13 06:52:21 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,16 +1,20 @@
-// Налаштування та Ключіі
+// js/config.js
+
 export const KEYS = {
     NEWSDATA: "pub_22e4e8780f9349e7a64a65f886ecae3a",
     GNEWS: "988894076e3186f8fbd93db235ee6fe9",
-    TMDB: "4dac8d33b5f9ef7b7c69d94b3f9cd56b", // 👈 Тут була потрібна кома
-    GOOGLE_KEY: 'AIzaSyDimPr4gHrGlvniu8hau0A56w41AbyTarc', 
-    GOOGLE_CX: '51f68decc681b45e6' 
+    TMDB: "4dac8d33b5f9ef7b7c69d94b3f9cd56b",
+    GOOGLE_KEY: 'AIzaSyD_ipUid4VlDjd1jzg6u8pwEMIotWLnAoE', 
+    GOOGLE_CX: '51f68decc681b45e6',
+    // 👇 Вставте ваш токен Alloha сюди
+    ALLOHA: '04941a9a3ca3ac16e2b4327347bbc1' 
 };
 
 export const API_URLS = {
     newsdata: 'https://newsdata.io/api/1/news',
-    gnews: 'https://gnews.io/api/v4/search',
     tmdb: 'https://api.themoviedb.org/3',
-    tmdbImg: 'https://image.tmdb.org/t/p/w500', // 👈 І тут була потрібна кома
-    googleSearch: 'https://www.googleapis.com/customsearch/v1' 
+    tmdbImg: 'https://image.tmdb.org/t/p/w500',
+    googleSearch: 'https://www.googleapis.com/customsearch/v1',
+    // Базовий URL для API Alloha
+    allohaApi: 'https://api.alloha.tv' 
 };

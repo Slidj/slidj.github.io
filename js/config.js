fc8eb9cb29a684ae85095c17e8ed7ca5adaef18b
Slidj <78273709+Slidj@users.noreply.github.com>
2025-12-13 03:22:14 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 export const KEYS = {
     NEWSDATA: "pub_22e4e8780f9349e7a64a65f886ecae3a",
     GNEWS: "988894076e3186f8fbd93db235ee6fe9",
-    TMDB: "4dac8d33b5f9ef7b7c69d94b3f9cd56b"
+    TMDB: "4dac8d33b5f9ef7b7c69d94b3f9cd56b", // 👈 Тут була потрібна кома
     GOOGLE_KEY: 'AIzaSyD_ipUid4VlDjd1jzg6u8pwEMIotWLnAoE', 
     GOOGLE_CX: '51f68decc681b45e6' 
 };
@@ -11,7 +11,6 @@ export const API_URLS = {
     newsdata: 'https://newsdata.io/api/1/news',
     gnews: 'https://gnews.io/api/v4/search',
     tmdb: 'https://api.themoviedb.org/3',
-    tmdbImg: 'https://image.tmdb.org/t/p/w500'
+    tmdbImg: 'https://image.tmdb.org/t/p/w500', // 👈 І тут була потрібна кома
     googleSearch: 'https://www.googleapis.com/customsearch/v1' 
 };
-

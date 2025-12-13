a057ee55ec307e58d856bd6ae64d207158b781af
Slidj <78273709+Slidj@users.noreply.github.com>
2025-12-13 02:56:11 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,8 @@ export const KEYS = {
     NEWSDATA: "pub_22e4e8780f9349e7a64a65f886ecae3a",
     GNEWS: "988894076e3186f8fbd93db235ee6fe9",
     TMDB: "4dac8d33b5f9ef7b7c69d94b3f9cd56b"
+    GOOGLE_KEY: 'AIzaSyD_ipUid4VlDjd1jzg6u8pwEMIotWLnAoE', 
+    GOOGLE_CX: '51f68decc681b45e6' 
 };
 
 export const API_URLS = {
@@ -10,5 +12,6 @@ export const API_URLS = {
     gnews: 'https://gnews.io/api/v4/search',
     tmdb: 'https://api.themoviedb.org/3',
     tmdbImg: 'https://image.tmdb.org/t/p/w500'
+    googleSearch: 'https://www.googleapis.com/customsearch/v1' 
 };
 

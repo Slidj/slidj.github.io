33003cc98f800cbc4fcf5ec6d140458fa77b8407
Slidj <78273709+Slidj@users.noreply.github.com>
2025-12-14 23:10:59 +0200

    Add files via upload

--- js/state.js
+++ js/state.js
@@ -1,1 +1,11 @@
-
+export const state = {
+    currentTab: 'home',
+    feedMovies: [],      // Всі завантажені фільми
+    savedItems: [],      // Вибране
+    currentHeroMovie: null,
+    currentPage: 1,
+    isLoading: false,
+    currentGenre: '',
+    cachedKpId: null,
+    searchTimeout: null
+};
